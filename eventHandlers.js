1a166596be6e2217a87dabf23d6ceaaa1133901d
Edgar Barajas <ebarajas05@gmail.com>
2016-08-05 11:56:55 -0500

    modified the focus and onkey events to apply to all 3 cards

--- eventHandlers.js
+++ eventHandlers.js
@@ -2,80 +2,67 @@
 // change the width of the border to a higher value, 
 //and change the background color to any other color of your choosing.
 
+// --- The final IIFE should augment the object with two more functions. 
+// One function resets the border thickness and background color for each 
+// car element back to the original values. The other function changes the 
+// thickness of the border of a car element, and changes its background color. 
+// The function must accept two arguments.
+
+//    1. A car DOM element that was clicked on.
+//    1. A color name.
+
 function loadingevents () {
-
-
-// function loadingevents () {
   console.log("loaded after DOM")
+
   function cardMorph() {
     document.getElementById("loadCard").classList.toggle("cardToggle");
-  
-document.getElementById("loadedCard").addEventListener("click", cardMorph);
-}
-
-
-// 1. Also, on click of the car element, clear the value of
-// the text input in the navbar, and put the [cursor in the text input]
-// (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus).
-
-
-'ul'.onclick = function(event) {
-    event = event || window.event // cross-browser event
-    
-    if (event.stopPropagation) {
-        // W3C standard variant
-        event.stopPropagation()
-    } else {
-        // IE variant
-        event.cancelBubble = true
-    }
+    document.getElementById("loadedCard").addEventListener("click", cardMorph);
   }
 
-var target = event.target || event.srcElement
 
-// target.addEventListener('click', focusEvent);
+  // 1. Also, on click of the car element, clear the value of
+  // the text input in the navbar, and put the [cursor in the text input]
+  // (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus).
 
-// document.getElementById("descId").addEventListener('click', focusEvent);
 
-// this code in between this block works good to tag al UL elements
+  // this code in between this block works good to tag al UL elements for focus:
 
-var carD = document.getElementsByTagName('ul')
+  var carD = document.getElementsByTagName('ul');
 
-for(var i=0; i<carD.length; i++) {
-  carD[i].onclick = function(e) {
-    e = e || event
-    var target = e.target || e.srcElement
-
-    this.addEventListener('click', focusEvent);  
+  for(var i=0; i<carD.length; i++) {
+    carD[i].addEventListener('click', focusEvent);
   }
 
-};
-
-
-function focusEvent () {
+  function focusEvent () {
     document.getElementById("searchCrit").focus();
-  };
-
-// end of code block for focus ---
-
-var inputBox = document.getElementById("searchCrit");
-
-var descrip = document.getElementById("descId")
-
-for(var i=0; i<descrip.length; i++) {
-  descrip[i].onclick = function(e) {
-    e = e || event
-    var target = e.target || e.srcElement
-
-    this.addEventListener('click', inputBox.onkeyup);  
+    addKeyupListener(event.currentTarget)
   }
 
-}
+  // end of code block for focus ---
 
-inputBox.onkeyup = function(){
-    document.getElementById("descId").innerHTML = inputBox.value;
-};
+  // duplicates text entered into box into the DOM:
 
+  function addKeyupListener (carBox) {
+    var inputBox = document.getElementById("searchCrit");
+
+    inputBox.addEventListener('keyup', function () {
+      carBox.querySelector('.descrip').innerHTML = 'Description: ' + inputBox.value;
+    })
+  }
+  
+  // end of duplicating input box text 
+
+  function removeKeyupListener () {
+    // remove keyup event when another card is clicked and or
+    // modify button is clicked
+    if (true) {document.getElementById("searchCrit").addEventListener('click', clearBox)} 
+
+      else {}
+  }
+
+  function clearBox () {
+    searchCrit.value = '';
+  }
 }
 
 
--- iife.js
+++ iife.js
@@ -6,18 +6,18 @@ function populatePage (loadedInventory) {
   loadedInventory.forEach(function (car) {
     inventoryBox.innerHTML += 
     `<div class="col-md-4 success">
-      <ul id="loadedCard" class="list-unstyled card cardToggle">
-       <li><h2>${car.make}</h2></li>
-       <li><h3>${car.model}</h3></li>
-       <li><h5>${car.year}<h5></li>
-       <li><h5>${car.price}<h5></li>
-       <li><h5>${car.color}<h5></li>
-       <li><h5><u>${car.purchased}</u></h5></li>
-       <li id="descId" class="descrip">${car.description}</li>
+      <ul id="loadedCard" class="list-unstyled card cardToggle" style="border: 4px solid ${car.color}">
+       <li><h2>${car.image}</h2></li>
+       <li><h2>Make: ${car.make}</h2></li>
+       <li><h3>Model: ${car.model}</h3></li>
+       <li><h5>Year: ${car.year}<h5></li>
+       <li><h5>Price: ${car.price}<h5></li>
+       <li><h5>Color: ${car.color}<h5></li>
+       <li><h5><u>Available: ${car.purchased}</u></h5></li>
+       <li id="descId" class="descrip">Description: ${car.description}</li>
       </ul>
     </div>`
   })
-alert("domloaded")
 
 loadingevents ()
 
